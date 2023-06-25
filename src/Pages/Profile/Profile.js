c41cdb2f0c2e25742286c155be6bc3824728c52b
Eby-Tom <ebytomy7@gmail.com>
2023-06-25 10:54:00 +0530

    admin panel

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../../App'
+import { VerifiedIcon } from '@primer/octicons-react'
 
 const Profile = () => {
 
@@ -19,7 +20,13 @@ const Profile = () => {
         <div className='p-2'>
             <div className='d-flex p-2 py-5 bg-light rounded-3' style={{ flexDirection: "column", alignItems: "center" }}>
                 <div><img src="profile.png" height="150px" width="150px" style={{ borderRadius: "50%" }} /></div>
-                <b className='mt-2 fs-3'>{user.name}</b>
+                <div className='d-flex mt-2 align-items-center gap-2'>
+                    <b className='fs-3'>{user.name}</b>
+                    {
+                        user.isAdmin &&
+                        <VerifiedIcon fill={"blue"} size={20} />
+                    }
+                </div>
                 <b className='mb-3'><i>{user.email}</i></b>
                 <div onClick={signOut} className='bg-danger p-2 px-3 rounded-3 text-white'><b>Signout</b></div>
             </div>
